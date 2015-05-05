ae2ec911c65ed3c11860bb9a63e86614d436f90d
Jason Livesay <ithkuil@gmail.com>
2015-05-05 02:23:20 -0500

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+};
+
+function onError() {
+    alert('onError!');
+};
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
